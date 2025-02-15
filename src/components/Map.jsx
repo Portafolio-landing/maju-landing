a6e4be59ffd536225d2ea30f98c239577a7eaf2f
Marsi3116 <marsi3116@gmail.com>
2025-02-14 20:20:11 -0500

    feat: redireccion de ubicacion y numero

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,9 +11,16 @@ const Map = () => {
         attribution: "© OpenStreetMap contributors",
       }).addTo(map);
 
-      L.marker([-11.989842457771704, -77.07162078883616]).addTo(map)
+      // Crear marcador
+      const marker = L.marker([-11.989842457771704, -77.07162078883616])
+        .addTo(map)
         .bindPopup("¡Ven y descúbrenos!🧁")
         .openPopup();
+
+      // Agregar evento click al marcador para redirigir a Google Maps
+      marker.on("click", function () {
+        window.open("https://www.google.com/maps/place/EUROPOSTRES+MAJU/@-11.9898176,-77.0742235,17z/data=!3m1!4b1!4m6!3m5!1s0x9105ce5ba679629b:0xa934456619a82058!8m2!3d-11.9898176!4d-77.0716432!16s%2Fg%2F11b6vk2k1x?entry=ttu&g_ep=EgoyMDI1MDIxMi4wIKXMDSoASAFQAw%3D%3D", "_blank");
+      });
 
       return () => map.remove();
     }
